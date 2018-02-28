18d29dd10988960de036f5ed88d56f3be0eddf5a
Nycole <nycolealexandracastro@gmail.com>
2018-02-28 00:08:13 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :beers
+
+  validates :first_name, :last_name, presence: true, length: {in: 2..20}
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]+)\z/i
+  validates :email, presence: true, uniqueness: {case_sensitive: false},format: {with: EMAIL_REGEX}
 end
