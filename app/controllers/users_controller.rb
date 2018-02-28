bedff18c2968f52991167489a8c5da3bb0aa94ce
Alex Tran <lxdemikz@gmail.com>
2018-02-27 18:52:39 -0800

    added creation method for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to '/songs'
+    else
+        flash[:error]= @user.errors.full_messages
+        redirect_to :back
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:email,:password, :password_confirmation)
+  end
 end
